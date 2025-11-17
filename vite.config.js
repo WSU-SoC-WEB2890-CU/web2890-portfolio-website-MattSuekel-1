e814ef27e0d9f4e9a124be38b38cea21ce48487b
MattSuekel <matthew.suekel@gmail.com>
2025-11-16 19:34:54 -0700

    init

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,8 @@
-
-const path = require("path")
-const glob = require("glob")
-
-import { defineConfig } from "vite"
-import eslint from "@rollup/plugin-eslint"
-
-let input = {}
-
-glob
-  .sync("./src/**/*.html")
-  .map((file) => {
-    return [file.substring(`.${path.sep}src${path.sep}`.length), file]
-  })
-  .forEach((f) => {
-    input[f[0]] = f[1]
-  })
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  root: "src",
-  build: {
-    manifest: true,
-    outDir: "../dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input
-    },
-  },
-  server: {
-    port: 8080,
-    open: "/",
-  },
   plugins: [
-    {
-      ...eslint({
-        include: ["src/**/*.js"],
-      }),
-      enforce: "pre",
-      apply: "build",
-    },
+    tailwindcss(),
   ],
 })
