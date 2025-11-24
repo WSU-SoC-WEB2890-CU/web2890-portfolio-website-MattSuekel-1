ea5b3049ab7e99c8aac951537ce77276a1ada35f
MattSuekel <matthew.suekel@gmail.com>
2025-11-23 21:45:52 -0700

    finish about start works and contact

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: {
     host: true,
   },
-
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
 })
